b64c7c9a504e2a4df690573b0f3aad5c51d20f6a
Maryypp <marypvh26@gmail.com>
2024-05-28 10:57:13 -0600

    Condicional expresion

---
+++ src/Condicional_expresion.kt
@@ -0,0 +1,32 @@
+fun main() {
+    /*
+    //Problema 1
+    print("Ingrese el primer valor: ")
+    val valor1 = readln().toInt()
+    print("Ingrese el segundo valor:  ")
+    val valor2 = readln().toInt()
+    val mayor = if(valor1>valor2) valor1 else valor2
+    println("El mayor entre $valor1 y valor2 es $mayor")
+*/
+
+/*
+//Problema 2
+
+    print("Ingrese un valor entero: ")
+    val valor = readln().toInt()
+    val resultado = if (valor % 2 ==0){
+        print("Cuadrado: ")
+        valor * valor
+    }else{
+        print("Cubo: ")
+        valor * valor * valor
+    }
+print(resultado)
+ */
+
+    //Problema 3
+    print("Ingrese un numero entero entre el 1 y el 99: ")
+    val valor = readln().toInt()
+    val cant = if(valor >= 10) println("$valor tiene 2 digitos") else println("$valor tiene un digito")
+
+}
