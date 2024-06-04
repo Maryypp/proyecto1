909e6b1672eb4bfa3d26fe4bd121948fa93b2afa
Maryypp <marypvh26@gmail.com>
2024-06-04 08:23:34 -0600

    condicionales anidadas y compuestas

---
+++ src/Estructuras_condicionales_anidadas.kt
@@ -0,0 +1,87 @@
+fun main () {
+    /*
+   print("Ingrese primer nota: ")
+    val nota1 = readln().toInt()
+    print("Ingrese segunda nota: ")
+    val nota2 = readln().toInt()
+    print("Ingrese tercer nota: ")
+    val nota3 = readln().toInt()
+    val promedio = (nota1 + nota2 + nota3) /3
+    if(promedio >=7)
+        print("Promocionado")
+    else
+        if(promedio >=4)
+            print("Regular")
+    else
+        print("Libre")
+*/
+    /*
+    print("Ingrese primer nota: ")
+    val nota1 = readln().toInt()
+    print("Ingrese segunda nota: ")
+    val nota2 = readln().toInt()
+    print("Ingrese tercer nota: ")
+    val nota3 = readln().toInt()
+    val promedio = (nota1 + nota2 + nota3) /3
+    val estado = if(promedio >=7) "Promocionado" else if (promedio >=4) "Regular" else "Libre"
+    print("Estado del alumno $estado")
+
+*/
+    /*
+
+//Problema 1
+    print("Ingrese primer valor: ")
+    val valor1 = readln().toInt()
+    print("Ingrese segundo valor: ")
+    val valor2 = readln().toInt()
+    print("Ingrese tercer valor: ")
+    val valor3 = readln().toInt()
+    if (valor3 > valor2)
+        print("El mayor valor es $valor3")
+    else
+        if (valor2 > valor1)
+            print("El mayor valor es $valor2")
+        else
+            print("El mayor valor es $valor1")
+*/
+
+
+    /*
+//Problema 3
+    print("Ingrese un numero: ")
+val num = readln().toInt()
+    if(num >= 100)
+        print("Tiene 3 digitos")
+    else
+        if(num >=10)
+            print("Tiene 2 digitos")
+    else
+        if(num <= 9)
+            print("Tiene 1 digito")
+*/
+    //Problema 4
+
+    print("Ingrese la cantidad de preguntas que contesto: ")
+    val respu = readln().toInt()
+    print("Ingrese la cantidad de respuestas correctas: ")
+    val correct = readln().toInt()
+    val promedio: Float = correct / respu .toFloat()
+    if (promedio >= 0.9)
+        print("Nivel màximo")
+    if(promedio >= 0.75 && promedio < 0.9 )
+        print("Nivel medio")
+    if(promedio >= 0.50 && promedio< 0.75)
+        print("Nivel regular")
+    if(promedio < 0.50)
+        print("Fuera de nivel")
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/condiciones_compuestas.kt
@@ -0,0 +1,136 @@
+fun main() {
+/*
+ //1
+print("Ingrese primer valor:")
+    val num1 = readln().toInt()
+    print("Ingrese segundo valor:")
+    val num2 = readln().toInt()
+    print("Ingrese tercer valor:")
+    val num3 = readln().toInt()
+    if (num1 > num2 && num1 > num3)
+        print(num1)
+    else
+        if (num2 > num3)
+            print(num2)
+        else
+            print(num3);
+  */
+
+
+/*
+//2
+print("Ingrese día:")
+ val dia = readln().toInt()
+ print("Ingrese mes:")
+ val mes = readln().toInt()
+ print("Ingrese Año:")
+ val año = readln().toInt()
+ if (mes == 1 || mes == 2 || mes == 3)
+     print("Corresponde al primer trimestre");
+*/
+
+
+/*
+//3
+print("Ingrese día:")
+val dia = readln().toInt()
+print("Ingrese mes:")
+val mes = readln().toInt()
+print("Ingrese Año:")
+val año = readln().toInt()
+if ( mes == 12 && dia == 25)
+    print("Corresponde a navidad")
+ */
+
+
+/*
+//4
+print("Ingrese primer numero:")
+val num1 = readln().toInt()
+print("Ingrese segundo numero:")
+val num2 = readln().toInt()
+print("Ingrese tercer numero:")
+val num3 = readln().toInt()
+val cubo = num1 * num2 * num3
+if (num1 == num2 && num1 == num3)
+    print("El cubo de los numeros son: $cubo")
+*/
+
+
+/*
+//5
+print("Ingrese primer numero:")
+val num1 = readln().toInt()
+print("Ingrese segundo numero:")
+val num2 = readln().toInt()
+print("Ingrese tercer numero:")
+val num3 = readln().toInt()
+if (num1 < 10 && num2 < 10 && num3 <10)
+print("Todos son menores a diez")
+ */
+
+
+/*
+//6
+print("Ingrese primer numero:")
+val num1 = readln().toInt()
+print("Ingrese segundo numero:")
+val num2 = readln().toInt()
+print("Ingrese tercer numero:")
+val num3 = readln().toInt()
+if (num1 > 10 || num2 > 10 || num3 > 10)
+    print("Alguno es mayor a diez")
+*/
+
+
+/*
+//7
+print("Ingrese primer numero:")
+val num1 = readln().toInt()
+print("Ingrese segundo numero:")
+val num2 = readln().toInt()
+print("Ingrese tercer numero:")
+val num3 = readln().toInt()
+if (num1 < 10 || num2 < 10 || num3 < 10)
+    print("Alguno es menor a diez")
+*/
+
+
+/*
+//8
+print("Ingrese coordenada x:")
+val x = readln().toInt()
+print("Ingrese coordenada y:")
+val y = readln().toInt()
+if (x > 0 && y > 0)
+    print("Esta en el primer cuadrante")
+else
+    if (x < 0 && y > 0)
+        print("Esta en el segundo cuadrante")
+    else
+        if (x < 0 && y < 0)
+            print("Esta en el tercer cuadrante")
+        else
+            if (x > 0 && y < 0)
+                print("Esta en el cuarto cuadrante")
+            else
+                print("Esta en un eje")
+*/
+
+
+/*
+//9
+ print("Ingrese primer valor:")
+ val valor1 = readln().toInt()
+ print("Ingrese segundo valor:")
+ val valor2 = readln().toInt()
+ print("Ingrese tercer valor:")
+ val valor3 = readln().toInt()
+ val menor = if (valor1 < valor2 && valor1 < valor3)
+ valor1 else if (valor2 < valor3) valor2 else valor3
+ val mayor = if (valor1 > valor2 && valor1 > valor3)
+ valor1 else if (valor2 > valor3) valor2 else valor3
+ print("El mayor de la lista es $mayor y el menor $menor")
+*/
+
+}
