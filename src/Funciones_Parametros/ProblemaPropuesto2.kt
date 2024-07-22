de0596478e02b68537e69f9bb432957880c46ae3
Maryypp <marypvh26@gmail.com>
2024-07-22 13:07:48 -0600

    Funciones parametro

--- src/Funciones_Parametros/ProblemaPropuesto2.kt
+++ src/Funciones_Parametros/ProblemaPropuesto2.kt
@@ -1,24 +1,24 @@
 package Funciones_Parametros
 
-fun menormayor(num1: Int, num2: Int, num3: Int) {
+fun menomayo(num1: Int, num2: Int, num3: Int) {
     when {
         num1 < num2 && num1 < num3 -> {
             if (num2 < num3)
-                println("El orden de numeros de menor a mayor es: $num1 - $num2 - $num3")
+                println("Los numeros de menor a mayor: $num1 - $num2 - $num3")
             else
-                println("El orden de numeros de menor a mayor es: $num1 - $num3 - $num2")
+                println("Los numeros de menor a mayor: $num1 - $num3 - $num2")
         }
         num2 < num1 && num2 < num3 -> {
             if (num1 < num3)
-                println("El orden de numeros de menor a mayor es: $num2 - $num1 - $num3")
+                println("Los numeros de menor a mayor: $num2 - $num1 - $num3")
             else
-                println("El orden de numeros de menor a mayor es: $num2 - $num3 - $num1")
+                println("Los numeros de menor a mayor: $num2 - $num3 - $num1")
         }
         else -> {
             if (num1 < num2)
-                println("El orden de numeros de menor a mayor es: $num3 - $num1 - $num2")
+                println("Los numeros de menor a mayor: $num3 - $num1 - $num2")
             else
-                println("El orden de numeros de menor a mayor es: $num3 - $num2 - $num1")
+                println("Los numeros de menor a mayor: $num3 - $num2 - $num1")
         }
     }
 }
@@ -30,5 +30,5 @@ fun main() {
     val num2 = readln().toInt()
     print("Ingrese tercer valor:")
     val num3 = readln().toInt()
-    menormayor(num1, num2, num3)
+    menomayo(num1, num2, num3)
 }
