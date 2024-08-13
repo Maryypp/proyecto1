7e7278575b9189b82f1c5a4abbfcd89381253048
Maryypp <marypvh26@gmail.com>
2024-08-13 18:52:02 +0200

    POO llamadas metodoss

--- src/POO_llamada_metodos/Problema_propuesto.kt
+++ src/POO_llamada_metodos/Problema_propuesto.kt
@@ -2,19 +2,34 @@ package POO_llamada_metodos
 
 class Hijos(){
 
-    var prom = 0
-    fun Cargar(){
-        for (i in 1..5){
-            println("Ingrese la edad: ")
+    val edades = IntArray(5)
 
+    fun cargar() {
+        for (i in edades.indices) {
+            print("Ingrese la edad del hijo: ")
+            edades [i] = readln().toInt()
         }
-        mayorcito()
+        mayor()
         promedio()
     }
-    fun mayorcito(){
-
+    fun mayor() {
+        var mayor = edades[0]
+        for (i in edades.indices)
+            if (edades[i] > mayor)
+                mayor = edades[i]
+        println("La mayor edad es $mayor")
     }
+
     fun promedio() {
+        var suma = 0
+        for (i in edades.indices)
+            suma += edades[i]
+        val promedio = suma/edades.size
+        println("El promedio de edades de los hijos es: $promedio")
     }
+}
 
+fun main() {
+    val hijos = Hijos()
+    hijos.cargar()
 }
